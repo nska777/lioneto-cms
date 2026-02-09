8f4745daaa06dc97defc01f08ad96eee3a69ad15
Roman <freak-009@mail.com>
2026-02-10 00:59:22 +0500

    delete  admin.ts create admin.js and copy in config

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,15 @@
+// config/server.ts
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+
+  // Важно для работы за reverse-proxy (App Platform)
+  proxy: true,
+
+  // Публичный URL приложения (то, по чему ты заходишь снаружи)
+  url: env("PUBLIC_URL"),
+
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
