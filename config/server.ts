b150cea50cff0fb8c1047c60f0a520576edef449
Roman <freak-009@mail.com>
2026-02-10 09:26:53 +0500

    edit server ts

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,10 @@
-// config/server.ts
-
-
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
 
-
   proxy: true,
 
-  url: env("PUBLIC_URL"),
+  url: env("PUBLIC_URL", ""),
 
   app: {
     keys: env.array("APP_KEYS"),
